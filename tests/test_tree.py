457790db47f627e521583c2c649be7763fb9df76
Jack O'Sullivan <jackos1998@gmail.com>
2019-10-03 16:12:30 +0100

    tree: Expand parent assignment magic tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -3,3 +3,10 @@ from lca import Node
 def test_assignment():
     root = Node(123)
     assert root.parent == None
+
+    root.left = Node(456)
+    assert root.left.parent == root
+
+    root.left.right = Node(789)
+    assert root.left.right.parent == root.left
+    assert root.left.parent == root
