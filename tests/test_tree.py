46f201bd075287026341f8755a11006be1a79d66
Jack O'Sullivan <jackos1998@gmail.com>
2019-10-03 16:15:45 +0100

    tree: Test comparison operators

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -10,3 +10,18 @@ def test_assignment():
     root.left.right = Node(789)
     assert root.left.right.parent == root.left
     assert root.left.parent == root
+
+def test_comparison():
+    root = Node(123)
+    root.left = Node(456)
+    root.left.left = Node(456)
+    root.right = Node(789)
+    root.right.left = Node(456)
+    root.right.left.left = Node(999)
+
+    assert root.left > root
+    assert root.left == root.left.left
+    assert root.right > root.left.left
+    assert root.right.left.left.parent == root.left
+    assert root.left != root
+    assert not root.left != root.left.left
