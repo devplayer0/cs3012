16642a4e5c604cd304cd13dbb513920f9aca9279
Jack O'Sullivan <jackos1998@gmail.com>
2019-11-22 14:43:18 +0000

    github-vis: Basic data display

--- github-vis/access.js
+++ github-vis/access.js
@@ -1,12 +1,23 @@
 async function loadData(username) {
-    console.log(`username: ${username}`);
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    if (!res.ok) {
+        alert(`Failed to fetch info for ${username}`);
+        return;
+    }
+
+    const info = await res.json();
+    console.log(info);
+    $('#info-title').text(`User: ${info.login}`);
+    $('#info-avatar').attr('src', info.avatar_url);
+    $('#info-link').attr('href', info.html_url);
+    $('#info-name').text(info.name);
 }
 
 window.onload = e => {
-    const form = document.querySelector('#username-form');
-    form.addEventListener('submit', e => {
+    const form = $('#username-form');
+    form.submit(e => {
         e.preventDefault();
-        const data = new FormData(form);
+        const data = new FormData(form[0]);
 
         loadData(data.get('username'));
     });
