2c166bc72512aa79f62f7a33ade80bac6f156761
Jack O'Sullivan <jackos1998@gmail.com>
2019-11-22 16:15:06 +0000

    github-vis: Use Vue.js

---
+++ github-vis/vis.js
@@ -0,0 +1,61 @@
+async function loadData(username) {
+  const res = await fetch(`https://api.github.com/users/${username}`);
+  if (!res.ok) {
+    alert(`Failed to fetch info for ${username}`);
+    return;
+  }
+
+  const info = await res.json();
+  console.log(info);
+  $('#info-title').text(`User: ${info.login}`);
+  $('#info-avatar').attr('src', info.avatar_url);
+  $('#info-link').attr('href', info.html_url);
+  $('#info-name').text(info.name);
+}
+
+Vue.component('UsernameEntry', {
+  template: `
+    <input type="text" v-bind:placeholder="placeholder" v-model="value" />
+  `,
+  props: {
+    placeholder: String,
+    debounce: {
+      type: Number,
+      required: true
+    }
+  },
+  data: function() {
+    return {
+      value: ''
+    };
+  },
+  created: function() {
+    this.emitValue = _.debounce(this._emitValue, this.debounce);
+  },
+  methods: {
+    _emitValue: function(value) {
+      this.$emit('input', value);
+    }
+  },
+  watch: {
+    value: function(n, o) {
+      this.emitValue(n);
+    }
+  }
+});
+
+Vue.component('UserInfo', {
+  props: ['username'],
+  template: `
+    <div>
+      <h1>User: {{ username }}</h1>
+    </div>
+  `
+});
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    username: ''
+  }
+});
