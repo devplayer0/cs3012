cbb1ba7bac4c9adf62c8b5bd06b5eb6f2fc81a21
Jack O'Sullivan <jackos1998@gmail.com>
2019-11-24 16:19:18 +0000

    github-vis: Use Vue router

--- github-vis/vis.js
+++ github-vis/vis.js
@@ -1,23 +1,21 @@
-Vue.component('UsernameEntry', {
+Vue.component('DebouncedInput', {
   template: `
     <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text">https://github.com/</span>
+      <div v-if="prepend" class="input-group-prepend">
+        <span class="input-group-text">{{ prepend }}</span>
       </div>
-      <input class="form-control" type="text" :placeholder="placeholder" v-model="value" />
+      <input class="form-control" type="text" :placeholder="placeholder"
+             :value="value" @input="emitValue($event.target.value)" />
     </div>
   `,
   props: {
+    prepend: String,
     placeholder: String,
     debounce: {
       type: Number,
       required: true
-    }
-  },
-  data() {
-    return {
-      value: ''
-    };
+    },
+    value: String
   },
   created() {
     this.emitValue = _.debounce(this._emitValue, this.debounce);
@@ -80,15 +78,86 @@ Vue.component('UserInfo', {
       }
 
       const info = await res.json();
-      console.log(info);
       return info;
     }
   }
 });
 
+Vue.component('NotFound', {
+  template: `
+    <div>
+      <h1>Not Found</h1>
+      <p class="lead">Couldn't find a matching visualization, sorry.</p>
+      <code>¯\\_(ツ)_/¯</code>
+    </div>
+  `
+});
+
+Vue.component('Access', {
+  template: `
+    <div>
+      <h1>Basic API access</h1>
+      <p class="lead">Enter your GitHub username to retrieve some information about your profile.</p>
+
+      <debounced-input v-model="username" :debounce="750" prepend="https://github.com/" placeholder="some-coder">
+      </debounced-input>
+      <user-info v-show="username" v-bind:username="username"></user-info>
+    </div>
+  `,
+  data() {
+    return {
+      username: ''
+    };
+  },
+  created() {
+    this.username = this.$route.params.username;
+  },
+  watch: {
+    $route(to, from) {
+      this.username = to.params.username;
+    },
+    username(n, o) {
+      if (this.$route.params.username == n) {
+        return;
+      }
+
+      this.$router.push(`/access/${n}`);
+    }
+  }
+});
+
+Vue.component('DependencyGraph', {
+  template: `
+    <div>
+      <h1>Dependency graph</h1>
+      <p class="lead">Enter a repo's name to see its dependency graph.</p>
+
+      <debounced-input v-model="repo" :debounce="750" prepend="https://github.com/" placeholder="some-coder/some-repo">
+      </debounced-input>
+    </div>
+  `,
+  data() {
+    return {
+      repo: ''
+    };
+  }
+});
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/access' },
+
+    { path: '/access', component: Vue.component('Access') },
+    { path: '/access/:username', component: Vue.component('Access') },
+
+    { path: '/graph', component: Vue.component('DependencyGraph') },
+    { path: '/graph/:username/:repo', component: Vue.component('DependencyGraph') },
+
+    { path: '*', component: Vue.component('NotFound') }
+  ]
+});
+
 const vm = new Vue({
   el: '#app',
-  data: {
-    username: ''
-  }
+  router
 });
