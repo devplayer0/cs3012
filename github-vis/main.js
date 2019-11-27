af99f7402a84f1db6e82184b70709cec843b3fa5
devplayer0 <devplayer0@users.noreply.github.com>
2019-11-27 02:42:25 +0000

    Automated deployment: Wed Nov 27 02:42:25 UTC 2019 5643d11cbad77be6654816a8176da776b8a34507

---
+++ github-vis/main.js
@@ -0,0 +1,18 @@
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/access' },
+
+    { name: 'accessBlank', path: '/access', component: Vue.component('AccessView') },
+    { name: 'access', path: '/access/:username', component: Vue.component('AccessView') },
+
+    { name: 'graphBlank', path: '/graph', component: Vue.component('GraphView') },
+    { name: 'graph', path: '/graph/:repo', component: Vue.component('GraphView') },
+
+    { path: '*', component: Vue.component('NotFound') }
+  ]
+});
+
+const vm = new Vue({
+  el: '#app',
+  router
+});
