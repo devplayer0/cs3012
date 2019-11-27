3bc65bc7a93bec7436ea5de253e81b64cda56f62
Jack O'Sullivan <jackos1998@gmail.com>
2019-11-27 02:34:44 +0000

    github-vis: Add some comments and info

--- github-vis/components.js
+++ github-vis/components.js
@@ -153,7 +153,7 @@ Vue.component('DependencyGraph', {
       <div v-show="error" class="alert alert-danger" role="alert">
         Failed to retrieve dependency graph for '{{ repo }}'
       </div>
-      <svg width="100%" height="720"></svg>
+      <svg width="100%" height="680"></svg>
     </div>
   `,
   data() {
@@ -180,12 +180,15 @@ Vue.component('DependencyGraph', {
       .attr('viewBox', [-w/2, -h/2, w, h]);
 
     this.simulation = d3.forceSimulation()
+      // Force linking everything together
       .force('link',d3.forceLink()
         .id(d => d.name)
+        // Using a separate repulsion force
         .distance(0)
         .strength(1))
       .force('repulsion', d3.forceManyBody()
         .strength(-700))
+      // Push everything towards the centre
       .force('cx', d3.forceX())
       .force('cy', d3.forceY())
       .on('tick', this.tick);
@@ -193,6 +196,7 @@ Vue.component('DependencyGraph', {
     this.reloadData(this.repo);
   },
   methods: {
+    // Find the point of intersection between the edge of the target dependency's circle and the link
     applyDependencyIndicator(d) {
       let vx = d.target.x - d.source.x;
       let vy = d.target.y - d.source.y;
@@ -211,20 +215,19 @@ Vue.component('DependencyGraph', {
       }
 
       this.$emit('load-start');
-      const data = await githubNLDependencyGraph('REDACTED', repo, this.depth);
-      //const data = JSON.parse('{"nodes":[{"stars":0,"forks":0,"fx":0,"fy":0,"name":"devplayer0/cs3012"},{"stars":962,"forks":102,"name":"nedbat/coveragepy"},{"stars":5142,"forks":1225,"name":"pytest-dev/pytest"},{"stars":180,"forks":20,"name":"untitaker/python-atomicwrites"},{"stars":2866,"forks":203,"name":"python-attrs/attrs"},{"stars":1811,"forks":139,"name":"tartley/colorama"},{"stars":7,"forks":0,"name":"calvinchengx/python-mock"},{"stars":1117,"forks":111,"name":"erikrose/more-itertools"},{"stars":137,"forks":93,"name":"pypa/packaging"},{"stars":496,"forks":104,"name":"pyparsing/pyparsing"},{"stars":586,"forks":137,"name":"benjaminp/six"},{"stars":55,"forks":12,"name":"mcmtroffaes/pathlib2"},{"stars":445,"forks":57,"name":"benhoyt/scandir"},{"stars":363,"forks":57,"name":"pytest-dev/pluggy"},{"stars":29,"forks":43,"name":"pytest-dev/py"},{"stars":146,"forks":24,"name":"jquast/wcwidth"},{"stars":901,"forks":227,"name":"yaml/pyyaml"},{"stars":250,"forks":130,"name":"PyCQA/astroid"},{"stars":281,"forks":24,"name":"jquast/blessed"},{"stars":6873,"forks":522,"name":"docopt/docopt"},{"stars":69,"forks":12,"name":"landscapeio/dodgy"},{"stars":5,"forks":0,"name":"certik/enum34"},{"stars":251,"forks":23,"name":"timothycrosley/deprecated.frosted"},{"stars":77,"forks":39,"name":"phihag/ipaddress"},{"stars":658,"forks":119,"name":"pytest-dev/pytest-cov"},{"stars":257,"forks":121,"name":"pytest-dev/pytest-html"},{"stars":13642,"forks":728,"name":"psf/black"},{"stars":8789,"forks":1311,"name":"aio-libs/aiohttp"},{"stars":137,"forks":25,"name":"aio-libs/aiohttp-cors"},{"stars":487,"forks":57,"name":"ActiveState/appdirs"},{"stars":8473,"forks":835,"name":"pallets/click"},{"stars":379,"forks":37,"name":"ericvsmith/dataclasses"},{"stars":2,"forks":11,"name":"onlytiancai/flake8"},{"stars":339,"forks":19,"name":"PyCQA/flake8-bugbear"},{"stars":88,"forks":13,"name":"ambv/flake8-mypy"},{"stars":0,"forks":1,"name":"chriskuehl/pre-commit"},{"stars":2451,"forks":526,"name":"PyCQA/pylint"},{"stars":1,"forks":0,"name":"Distrotech/setuptools"},{"stars":2,"forks":0,"name":"msabramo/tox"},{"stars":2,"forks":0,"name":"cheshire/virtualenv"}],"links":[{"source":"devplayer0/cs3012","target":"nedbat/coveragepy"},{"source":"devplayer0/cs3012","target":"pytest-dev/pytest"},{"source":"pytest-dev/pytest","target":"untitaker/python-atomicwrites"},{"source":"pytest-dev/pytest","target":"python-attrs/attrs"},{"source":"pytest-dev/pytest","target":"tartley/colorama"},{"source":"tartley/colorama","target":"calvinchengx/python-mock"},{"source":"pytest-dev/pytest","target":"erikrose/more-itertools"},{"source":"pytest-dev/pytest","target":"pypa/packaging"},{"source":"pypa/packaging","target":"pyparsing/pyparsing"},{"source":"pypa/packaging","target":"benjaminp/six"},{"source":"pytest-dev/pytest","target":"mcmtroffaes/pathlib2"},{"source":"mcmtroffaes/pathlib2","target":"calvinchengx/python-mock"},{"source":"mcmtroffaes/pathlib2","target":"benhoyt/scandir"},{"source":"mcmtroffaes/pathlib2","target":"benjaminp/six"},{"source":"pytest-dev/pytest","target":"pytest-dev/pluggy"},{"source":"pytest-dev/pytest","target":"pytest-dev/py"},{"source":"pytest-dev/pytest","target":"jquast/wcwidth"},{"source":"jquast/wcwidth","target":"yaml/pyyaml"},{"source":"jquast/wcwidth","target":"PyCQA/astroid"},{"source":"jquast/wcwidth","target":"jquast/blessed"},{"source":"jquast/wcwidth","target":"docopt/docopt"},{"source":"jquast/wcwidth","target":"landscapeio/dodgy"},{"source":"jquast/wcwidth","target":"certik/enum34"},{"source":"jquast/wcwidth","target":"timothycrosley/deprecated.frosted"},{"source":"jquast/wcwidth","target":"phihag/ipaddress"},{"source":"devplayer0/cs3012","target":"pytest-dev/pytest-cov"},{"source":"pytest-dev/pytest-cov","target":"nedbat/coveragepy"},{"source":"pytest-dev/pytest-cov","target":"pytest-dev/pytest"},{"source":"devplayer0/cs3012","target":"pytest-dev/pytest-html"},{"source":"pytest-dev/pytest-html","target":"psf/black"},{"source":"psf/black","target":"aio-libs/aiohttp"},{"source":"psf/black","target":"aio-libs/aiohttp-cors"},{"source":"psf/black","target":"ActiveState/appdirs"},{"source":"psf/black","target":"pallets/click"},{"source":"psf/black","target":"nedbat/coveragepy"},{"source":"psf/black","target":"ericvsmith/dataclasses"},{"source":"psf/black","target":"onlytiancai/flake8"},{"source":"psf/black","target":"PyCQA/flake8-bugbear"},{"source":"psf/black","target":"ambv/flake8-mypy"},{"source":"pytest-dev/pytest-html","target":"onlytiancai/flake8"},{"source":"pytest-dev/pytest-html","target":"chriskuehl/pre-commit"},{"source":"chriskuehl/pre-commit","target":"PyCQA/astroid"},{"source":"chriskuehl/pre-commit","target":"nedbat/coveragepy"},{"source":"chriskuehl/pre-commit","target":"onlytiancai/flake8"},{"source":"chriskuehl/pre-commit","target":"calvinchengx/python-mock"},{"source":"chriskuehl/pre-commit","target":"PyCQA/pylint"},{"source":"chriskuehl/pre-commit","target":"pytest-dev/pytest"},{"source":"chriskuehl/pre-commit","target":"Distrotech/setuptools"},{"source":"pytest-dev/pytest-html","target":"pytest-dev/pytest"},{"source":"pytest-dev/pytest-html","target":"msabramo/tox"},{"source":"msabramo/tox","target":"pytest-dev/py"},{"source":"msabramo/tox","target":"msabramo/tox"},{"source":"msabramo/tox","target":"cheshire/virtualenv"}]}');
+      const data = await githubNLDependencyGraph('%%GRAPHQL_TOKEN%%', repo, this.depth);
       if (!data) {
         this.$emit('load-end');
         this.error = true;
         this.updateData([], []);
         return;
       }
-      //await sleep(500);
       this.$emit('load-end');
 
       this.updateData(data.nodes, data.links);
     },
 
+    // Update values necessary for simulation ticks (aka just positional stuff)
     tick() {
       this.svg
         .selectAll('.node')
@@ -251,6 +254,7 @@ Vue.component('DependencyGraph', {
               .attr('cx', d => d.px)
               .attr('cy', d => d.py));
     },
+    // Handle node drag events
     drag() {
       return d3.drag()
         .on('start', d => {
@@ -258,6 +262,7 @@ Vue.component('DependencyGraph', {
             return;
           }
           if (!d3.event.active) {
+            // Give the simulation a kick when we've just started dragging
             this.simulation
               .alphaTarget(0.3)
               .restart();
@@ -271,6 +276,7 @@ Vue.component('DependencyGraph', {
             return;
           }
 
+          // Temporarily assigning a fixed position will force the simulation to move everything else
           d.fx = d3.event.x;
           d.fy = d3.event.y;
         })
@@ -279,21 +285,25 @@ Vue.component('DependencyGraph', {
             return;
           }
           if (!d3.event.active) {
+            // Let the simulation rest after drag is finished
             this.simulation
               .alphaTarget(0);
           }
 
+          // Hand control of the node's position back to the simulation by unfixing it
           d.fx = null;
           d.fy = null;
         });
     },
     updateData(nodes, links) {
+      // Logarithmically scale the radius of the nodes based on stars
       const maxStars = _.max(_.map(nodes, n => n.stars));
       this.radiusScale = d3.scaleLog()
         .domain([1, maxStars])
         .clamp(true)
         .range([5, 13]);
 
+      // Logarithmically scale the repulsion of the nodes based on radius (so also stars)
       const repulsionScale = d3.scaleLog()
         .domain([1, maxStars])
         .clamp(true)
@@ -305,9 +315,12 @@ Vue.component('DependencyGraph', {
       this.simulation
         .force('repulsion')
           .strength(d => repulsionScale(d.name.length));
+
+      // We've just updated the data, give the simulation a kick
       this.simulation
         .alphaTarget(0.3)
         .restart();
+      // Calm everything down after a short while
       setTimeout(() => this.simulation.alphaTarget(0), 500);
 
       this.svg
@@ -332,7 +345,9 @@ Vue.component('DependencyGraph', {
                 .attr('r', 2.5)
                 .attr('fill', 'red')));
 
+      // Only display the repo's name (not the owner) to reduce clutter
       const repoDisplay = d => d.name.split('/')[1];
+      // Logarithmically scale the colour of each node based on forks with a GitHub-style pale yellow -> dark green
       const forkScale = d3.scaleLog()
         .domain([1, _.max(_.map(nodes, n => n.forks))])
         .clamp(true)
@@ -361,6 +376,8 @@ Vue.component('DependencyGraph', {
                 .style('user-select', 'none')
                 .style('font-size', '0.8rem')
                 .text(repoDisplay)),
+            // We can re-use SVG elements for perfomance when the data changes - only need to float the nodes to the top
+            // and update values depending on the data
             update => update
               .raise()
               .call(update => update
